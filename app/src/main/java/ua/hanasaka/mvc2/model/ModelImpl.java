f0c073b95a985cf76b5009affd6f96ff09bff702
hanasaka <hanasaka@bitbucket.org>
2017-05-15 19:08:37 +0300

    added model package

---
+++ app/src/main/java/ua/hanasaka/mvc2/model/ModelImpl.java
@@ -0,0 +1,17 @@
+package ua.hanasaka.mvc2.model;
+
+import java.util.List;
+
+import rx.Observable;
+import ua.hanasaka.mvc2.model.data.Repo;
+
+/**
+ * Created by Oleksandr Molodykh on 15.05.2017.
+ */
+
+public class ModelImpl implements Model {
+    @Override
+    public Observable<List<Repo>> getRepoList(String name) {
+        return null;
+    }
+}
